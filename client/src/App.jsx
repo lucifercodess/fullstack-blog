7d26524c36aa20da06d486cd4c29e29cc976dccc
lucifercodess <ayush.dubey0000444404@gmail.com>
2024-08-01 19:23:40 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home"
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path = "/" element= {<Home/>}/>
+        <Route path = "/about" element= {<About/>}/>
+        <Route path= "/sign-in" element= {<SignIn/>}/>
+        <Route path= "/dashboard" element= {<Dashboard/>}/>
+        <Route path = "/sign-up" element= {<SignUp/>}/>
+        <Route path = "/projects" element= {<Projects/>}/>
+      </Routes>
+    </div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from'react-router-dom' 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* wrapped the app with browser router */}
+    <BrowserRouter> 
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
